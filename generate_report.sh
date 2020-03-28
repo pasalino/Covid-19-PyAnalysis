a19315f3d482ba7ee64ade8d4657c890e385599f
Pasqualino de Simone <pasalino@gmail.com>
2020-03-28 16:18:16 +0100

    refactor code and add autogenerate pdf

--- generate_report.sh
+++ generate_report.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-mkdir -p reports
-
-jupyter nbconvert ./Covid-19.ipynb --TemplateExporter.exclude_input=True --TemplateExporter.exclude_output_prompt=True --TemplateExporter.exclude_input_prompt=True
 current_date=$(date +'%y-%m-%d')
+current_file="$current_date-Covid-19-Report.pdf"
+echo "$current_file"
+mkdir -p PDF
 
-mv Covid-19.html reports/$current_date-Covid-19-Report.html
-cp custom.css reports/custom.css
-git add .
-git commit -m "Report of $curent_date"
+jupyter nbconvert ./Covid-19.ipynb --TemplateExporter.exclude_input=True --to=pdf --TemplateExporter.exclude_output_prompt=True --TemplateExporter.exclude_input_prompt=True
+
+mv Covid-19.pdf "PDF/$current_file"
+
+git add PDF/$current_file
+git commit -m "Report of $(date +'%d-%m-%Y')"
 git push
